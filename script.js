8e98d534653c7dcf7c54ed0242b93c7b517eb933
Josephine Dorado <josephine@funksoup.com>
2019-10-01 20:25:57 -0400

    enable create button

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
 
-  		let customers = [
-   		   { id: 1, name: 'moe', email: 'moe@gmail.com', isVIP: true},
-    	 	{ id: 2, name: 'larry', isVIP: true, email: 'larry@gmail.com'},
-     		{ id: 4, name: 'shep', email: 'shep@gmail.com'},
+let customers = [
+    { id: 1, name: 'moe', email: 'moe@gmail.com', isVIP: true},
+    { id: 2, name: 'larry', isVIP: true, email: 'larry@gmail.com'},
+    { id: 4, name: 'shep', email: 'shep@gmail.com'},
      ];
 
+const selectorInit = () => {
 
-     	const createButton = document.querySelector('.buttonCreate');
+     	const createButton = document.querySelector('#buttonCreate');
      	const custName = document.querySelector('input[name="custName"]');
      	const custEmail = document.querySelector('input[name="custEmail"]');
  		const checkBox = document.querySelector('input[name="isVIP"]');
@@ -16,47 +17,61 @@
  		const customerRows = document.querySelector('.customerRows');
 
 
- 		customers.forEach( customer => customerRows.append(customer)
- 			);
+ 		// customers.forEach( customer => customerRows.append(customer)
+ 		// 	);
  
  
- 		createButton.addEventListener('click', (ev) => {
+ 		// createButton.addEventListener('click', (ev) => {
 
- 		})
+ 		// })
 
+        let nameValid = false;
+        let emailValid = false;
 
  		 custName.addEventListener('keyup', (ev) => {
-
+         
  		 	if(!ev.target.value) {
  		 		errorMessageName.innerHTML = 'name required';
  		 	} else {
  		 		errorMessageName.innerHTML = '';
-
+                nameValid = true;
  		 	}
 
  		 })
 
  		 custEmail.addEventListener('keyup', (ev) => {
+           console.log(ev);
 
  		 	if(!ev.target.value) {
  		 		errorMessageEmail.innerHTML = 'email required';
- 		 	} 
-
- 		 	if(!ev.target.value.includes('@')) {
+ 		 	} else if(!ev.target.value.includes('@')) {
  		 		errorMessageEmail.innerHTML = 'email must contain an @';
  		 	} 
  		 	else {
  		 		errorMessageEmail.innerHTML = '';
+                emailValid = true;
  		 	}
 
+            if (nameValid == true && emailValid == true) {
+                createButton.disabled = false;
+            }
+
  		 })
+
+
+
+
 
 
 
 
     
 
-    	if (isVIP.checked) {
+    	// if (isVIP.checked) {
 
-    	}
+    	// }
+
+    }
+
+    document.addEventListener("DOMContentLoaded", selectorInit);
 
